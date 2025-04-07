b6e496cddf718095ca660cefeeec9ca7928a57e0
Felix Kettnaker <felix.kettnaker@batix.com>
2025-04-07 21:34:13 +0200

    actually good AI integration plugin

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -14,6 +14,23 @@ return {
     },
   },
 
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "claude-3.7-sonnet",
+              },
+            },
+          })
+        end,
+      },
+    },
+  },
+
   { -- nvim-surround
     "kylechui/nvim-surround",
     opts = {
